4b36c15b518d72dd635b1531c1bb3b2334ec604c
Spark <50857240+sparkdoaz@users.noreply.github.com>
2021-10-18 15:44:07 +0800

    First Commit

---
+++ DIY-Combine-ZeroToOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DIY-Combine-ZeroToOne
+//
+//  Created by 黃建程 on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DIY-Combine-ZeroToOne/DIY_Combine_ZeroToOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIY_Combine_ZeroToOneApp.swift
+//  DIY-Combine-ZeroToOne
+//
+//  Created by 黃建程 on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct DIY_Combine_ZeroToOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
